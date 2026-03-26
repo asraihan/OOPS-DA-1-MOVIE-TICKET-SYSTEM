20d06e0169dd140414bff357acf2e31850d14957
asraihan <asraihanofficial133@gmail.com>
2026-03-26 21:19:29 +0530

    Delete run_server.bat

--- run_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Infiniplex Wasm Local Server...
-echo.
-echo ==============================================
-echo [!] Navigating to localhost:8000 in 3 seconds
-echo ==============================================
-timeout /t 3 /nobreak >nul
-start http://localhost:8000/
-python -m http.server 8000
